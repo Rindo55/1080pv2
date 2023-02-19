d6b42049942ac0a450aa2a80155a17e708d1bd1d
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-02-19 21:23:28 +0530

    Update uploader.py

--- main/modules/uploader.py
+++ main/modules/uploader.py
@@ -116,21 +116,10 @@ async def upload_video(msg: Message,file,id,tit,name,ttl):
         shorten_url = f"{da_url}shorten"
         response = requests.get(shorten_url, params={"url": url})     
         nyaa_text = response.text.strip()                                     
-        server = requests.get(url="https://api.gofile.io/getServer").json()["data"]["server"]
-        upload = requests.post(url=f"https://{server}.gofile.io/uploadFile",files={"upload_file": open(file, "rb")}).json()
-        directlink = upload["data"]["directLink"]    
-        gotn_url = f"https://tnlink.in/api?api=fea911843f6e7bec739708f3e562b56184342089&url={directlink}&format=text"
-        gofinal = requests.get(gotn_url)
-        go_text = gofinal.text
-        gourl = go_text
-        gofile_url = f"{da_url}shorten"
-        goresponse = requests.get(gofile_url, params={"url": gourl})
-        gofuk_text = response.text.strip()
         output = f"""
 {gcaption}
 ━━━━━━━━━━━━━━━━━━━
-[🔗Download Link 1]({nyaa_text})
-[🔗Download Link 2]({gofuk_text}"""
+[🔗Download Link 1]({nyaa_text})"""
         daze = await x.edit(output, parse_mode = "markdown")
 
     except Exception:
